1868c697427a54333627bae56fe24a7a2070f451
oruuser <oruuser@oruusers-MacBook-Pro.local>
2013-10-28 14:14:11 +0000

    micro post app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Micropostsapp::Application.config.secret_key_base = 'b8a8f23bd5d4a7d34ea8cbb52efb1dc347976b657212f0871b10d594b4753edbb3ab4d94c02461053745f932183ddb45c821c2c164e10788446250192d91918e'
